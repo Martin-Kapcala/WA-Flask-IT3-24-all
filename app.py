9edbb105d47c5b07deb9bdf0bba3ca410acf6c26
Martin-Kapcala <martin.kapcala@vitkovickastredni.cz>
2024-10-30 17:18:37 +0100

    create new HTML
    Přidány nové HTML soubory propojené

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ from flask import Flask, render_template
 from general.general import general_bp
 from products.products import products_bp
 from api.api import api_bp
+
 app = Flask(__name__)
 
 #Registrace blueprintu - takto o něm bude Flask vědět
 app.register_blueprint(general_bp)
 app.register_blueprint(products_bp, url_prefix="/products")
-app.register_blueprint(api_bp)
+#app.register_blueprint(api_bp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
