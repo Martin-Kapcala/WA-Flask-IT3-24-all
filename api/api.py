70d95ecdd218438c55341ff7a316ac602fe85f77
Martin-Kapcala <martin.kapcala@vitkovickastredni.cz>
2024-11-25 12:40:38 +0100

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, Blueprint
-
+import requests, json
 
 api_bp = Blueprint('api_bp', __name__,
     template_folder='templates',
     static_folder='static', static_url_path='assets')
+
+#URL prefix API
+URL_PATH_API = "https://api.escuelajs.co/api/v1"
+
+def GetAllProducts():
+    result = requests.get(f"{URL_PATH_API}/products")
+
+    return json.loads(result.text)
